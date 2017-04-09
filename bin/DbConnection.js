b4f78f896b63d07a2b71104ea44ca498340c8ee7
kenta <kenta@kenta.localdomain>
2017-04-09 12:39:48 +0900

    expressを導入。ディレクトリ構造を大幅に変更

--- bin/DbConnection.js
+++ bin/DbConnection.js
@@ -36,12 +36,6 @@ function handleDisconnect() {
         }
     });
 }
-mysql.configure({
-		host     : 'localhost'
-	,	user     : 'sample'
-	,	password : 'Sample1!'
-	,	database: 'SampleDB040'
-})
 
 console.log('DB connected');
 
