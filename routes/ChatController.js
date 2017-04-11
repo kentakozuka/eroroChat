03eae14047794f94eafae5e7c3f921f6d271497d
kenta <kenta@kenta.localdomain>
2017-04-12 07:36:15 +0900

    session

--- routes/ChatController.js
+++ routes/ChatController.js
@@ -11,7 +11,6 @@
 var ChatController = function(app, http, CommonConst, DbConnection, io){
 
 	console.log('▲▲▲' + DbConnection);
-	var io		= require('socket.io')(http);
 
 	//ユーザ数を格納する変数
 	var userCnt = {
@@ -28,126 +27,125 @@ var ChatController = function(app, http, CommonConst, DbConnection, io){
 			res.redirect(CommonConst.PAGE_ID_USER_LOG_IN);
 			return;
 		}
+		res.redirect(CommonConst.PAGE_ID_CHAT);
+	});
 	
-		//socket.ioに接続された時に動く処理
-		io.on('connection', function(socket) {
+	//socket.ioに接続された時に動く処理
+	io.on('connection', function(socket) {
+	
+		//debug
+		console.log('%s さんが接続しました。', req.session.user.user_name);
+	
+		//デフォルトのチャンネル
+		var channel = 'A';
+		//Roomを初期化
+		socket.join(channel);
+		//アクセス時はデフォルトのチャンネルなので、そのユーザをカウント
+		userCnt.a++;
+		//全ユーザ上のユーザ数を更新
+		io.emit('user cnt', userCnt);
 		
-			//debug
-			console.log('%s さんが接続しました。', req.session.user.user_name);
+		//DBから今までのメッセージを取ってきて自分だけに表示
+	    DbConnection.query('SELECT * from t_comment', function(err, rows, fields) {
+	        if (err) {
+	            console.log('error: ', err);
+	            throw err;
+	        }
+			//「ようこそ」と「ID」を自分の画面だけに表示
+			socket.emit('welcome', req.session.user.user_name, rows);
+			socket.emit('get id', socket.id);
 		
-			//デフォルトのチャンネル
-			var channel = 'A';
-			//Roomを初期化
-			socket.join(channel);
-			//アクセス時はデフォルトのチャンネルなので、そのユーザをカウント
-			userCnt.a++;
-			//全ユーザ上のユーザ数を更新
-			io.emit('user cnt', userCnt);
-			
-			//DBから今までのメッセージを取ってきて自分だけに表示
-		    DbConnection.query('SELECT * from t_comment', function(err, rows, fields) {
-		        if (err) {
-		            console.log('error: ', err);
-		            throw err;
-		        }
-				//「ようこそ」と「ID」を自分の画面だけに表示
-				socket.emit('welcome', req.session.user.user_name, rows);
-				socket.emit('get id', socket.id);
-			
-				//接続時に同じチャンネルの人に入室を伝える
-				socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが入室しました！', 'system'); 
-		    });
-			
-		
-			/**
-			 * 'message'イベント関数
-			 * 同じチャンネルの人にメッセージを送る
-			 * @param	String	msj	ユーザが送信したメッセージ
-			 **/
-			socket.on('message', function(msj) {
-				io.sockets.in(channel).emit('message', req.session.user.user_name, msj);
-		
-				//DBに保存
-				DbConnection.query(
-						'INSERT INTO t_comment SET ?'
-					,	{
-								user_name: req.session.user.user_name
-							,	comment: msj
-						}
-					,	function(err, result) {
-							if (err) throw err;
-							console.log(result.insertId);
-						}
-				);
-			});
+			//接続時に同じチャンネルの人に入室を伝える
+			socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが入室しました！', 'system'); 
+	    });
+	
+		/**
+		 * 'message'イベント関数
+		 * 同じチャンネルの人にメッセージを送る
+		 * @param	String	msj	ユーザが送信したメッセージ
+		 **/
+		socket.on('message', function(msj) {
+			io.sockets.in(channel).emit('message', req.session.user.user_name, msj);
+	
+			//DBに保存
+			DbConnection.query(
+					'INSERT INTO t_comment SET ?'
+				,	{
+							user_name: req.session.user.user_name
+						,	comment: msj
+					}
+				,	function(err, result) {
+						if (err) throw err;
+						console.log(result.insertId);
+					}
+			);
+		});
 
-			
-			/**
-			 * 'disconnect'イベント関数
-			 * 接続が切れた時に動く
-			 * 接続が切れたIDを全員に表示
-			 * @param	String	e
-			 **/
-			socket.on('disconnect', function(e) {
-				console.log('%s さんが退室しました。', req.session.user.user_name);
-				if (channel === 'A') {
-					userCnt.a--;
-			
-				} else if(channel === 'B') {
-					userCnt.b--;
-				} else {
-					userCnt.kentaBot--;
+		/**
+		 * 'disconnect'イベント関数
+		 * 接続が切れた時に動く
+		 * 接続が切れたIDを全員に表示
+		 * @param	String	e
+		 **/
+		socket.on('disconnect', function(e) {
+			console.log('%s さんが退室しました。', req.session.user.user_name);
+			if (channel === 'A') {
+				userCnt.a--;
+		
+			} else if(channel === 'B') {
+				userCnt.b--;
+			} else {
+				userCnt.kentaBot--;
+			}
+			//アクティブユーザを更新
+			io.emit('user cnt', userCnt);
+		});
+		
+		/**
+		 * 'disconnect'イベント関数
+		 * チャンネルを変えた時に動く
+		 * 今いるチャンネルを出て、選択されたチャンネルに移動する
+		 * @param	String	e
+		 **/
+		socket.on('change channel', function(newChannel) {
+			//ルーム内の自分以外
+			socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが退室しました！', 'system');
+			if (newChannel === 'A') {
+				++userCnt.a;
+				if (userCnt.b > 0) {
+					--userCnt.b;
 				}
-				//アクティブユーザを更新
-				io.emit('user cnt', userCnt);
-			});
-			
-			/**
-			 * 'disconnect'イベント関数
-			 * チャンネルを変えた時に動く
-			 * 今いるチャンネルを出て、選択されたチャンネルに移動する
-			 * @param	String	e
-			 **/
-			socket.on('change channel', function(newChannel) {
-				//ルーム内の自分以外
-				socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが退室しました！', 'system');
-				if (newChannel === 'A') {
-					++userCnt.a;
-					if (userCnt.b > 0) {
-						--userCnt.b;
-					}
-					if (userCnt.kentaBot > 0) {
-						--userCnt.kentaBot;
-					}
-				} else if (newChannel === 'B') {
-					++userCnt.b;
-					if (userCnt.a > 0) {
-						--userCnt.a;
-					}
-					if (userCnt.kentaBot > 0) {
-						--userCnt.kentaBot;
-					}
-				} else {
-					++userCnt.b;
-					if (userCnt.a > 0) {
-						--userCnt.a;
-					}
-					if (userCnt.b > 0) {
-						--userCnt.b;
-					}
+				if (userCnt.kentaBot > 0) {
+					--userCnt.kentaBot;
 				}
-				io.emit('user cnt', userCnt);
-				//チャンネルを去る
-				socket.leave(channel); 
-				//選択された新しいチャンネルのルームに入る
-				socket.join(newChannel); 
-				//今いるチャンネルを保存
-				channel = newChannel; 
-				//チャンネルを変えたこと自分に送信
-				socket.emit('change channel', channel); 
-				//ルーム内の自分以外
-				socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが入室しました！', 'system');
-			});
+			} else if (newChannel === 'B') {
+				++userCnt.b;
+				if (userCnt.a > 0) {
+					--userCnt.a;
+				}
+				if (userCnt.kentaBot > 0) {
+					--userCnt.kentaBot;
+				}
+			} else {
+				++userCnt.b;
+				if (userCnt.a > 0) {
+					--userCnt.a;
+				}
+				if (userCnt.b > 0) {
+					--userCnt.b;
+				}
+			}
+			io.emit('user cnt', userCnt);
+			//チャンネルを去る
+			socket.leave(channel); 
+			//選択された新しいチャンネルのルームに入る
+			socket.join(newChannel); 
+			//今いるチャンネルを保存
+			channel = newChannel; 
+			//チャンネルを変えたこと自分に送信
+			socket.emit('change channel', channel); 
+			//ルーム内の自分以外
+			socket.broadcast.to(channel).emit('message', req.session.user.user_name + 'さんが入室しました！', 'system');
 		});
 	});
 };
