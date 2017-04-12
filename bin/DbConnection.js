21a09ecd8d23a4f03d765f3fe4a386a0d5f76cd4
kenta <kenta@kenta.localdomain>
2017-04-12 23:08:06 +0900

    session

--- bin/DbConnection.js
+++ bin/DbConnection.js
@@ -12,33 +12,13 @@ var mysql	= require('mysql'			);
 //DBに接続する関数
 var pool;
 function handleDisconnect() {
+	/*
+	 * コネクションをプールする
+	 * 以下の方法だとうまくいかなかったので変更した
+	 * connection = mysql.createConnection(process.env.DATABASE_URL);
+	 * この外の関数もいらないか？
+	 */
 	pool = mysql.createPool(process.env.DATABASE_URL);
-//    console.log('DBに接続します');		
-//	connection = mysql.createConnection(process.env.DATABASE_URL);
-//    connection.connect(function(err) {              	
-//		// or restarting (takes a while sometimes).
-//        if (err) {                                     
-//            console.log('error when connecting to db:', err);
-//			// We introduce a delay before attempting to reconnect,
-//			// to avoid a hot loop, and to allow our node script to
-//			// process asynchronous requests in the meantime.
-//			// If you're also serving http, display a 503 error.
-//            setTimeout(handleDisconnect, 1000); 
-//        } else {
-//    		console.log('DBに接続しました');		
-//		}
-//    });
-//
-//	//エラーを受け取る
-//    connection.on('error', function(err) {
-//        console.log('DBエラー', err);
-//        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//			//再接続
-//            handleDisconnect();
-//        } else {
-//            throw err;
-//        }
-//    });
 }
 handleDisconnect();
 
