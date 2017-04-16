7b8c1e075b24dfbd414ac7be23b6e5525298ead6
kenta <kenta@kenta.localdomain>
2017-04-16 21:30:10 +0900

    hoge

--- doc/DB.sql
+++ doc/DB.sql
@@ -6,27 +6,42 @@ SET FOREIGN_KEY_CHECKS=0;
 
 -- テーブルの作成
 -- メッセージ
--- t_commentになってるけどt_messageに修正したい
 DROP TABLE IF EXISTS `t_comment`;
 CREATE TABLE t_comment (
 		`id`				SMALLINT(6)		NOT NULL AUTO_INCREMENT
 	,	`user_name`			VARCHAR(100)	DEFAULT NULL
 	,	`channel`			VARCHAR(100)	DEFAULT NULL
-	,	`comment`			VARCHAR(100)	DEFAULT NULL
+	,	`message`			VARCHAR(100)	DEFAULT NULL
+	,	delete_flog			TINYINT(1)
+	,	CREATED_DATETIME timestamp not null default current_timestamp
 	,	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT character set utf8mb4;
--- USER
+-- ユーザ
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE t_user (
 		id					INT AUTO_INCREMENT
 	,	user_name			VARCHAR(20)
 	,	user_password		VARCHAR(30)
 	,	email				VARCHAR(255)
+	,	delete_flog			TINYINT(1)
+	,	CREATED_DATETIME timestamp not null default current_timestamp
 	,	primary KEY (id)
 	,	INDEX(id)
 ) ENGINE=InnoDB character set utf8mb4;
 
--- インサート
+-- チャンネル
+DROP TABLE IF EXISTS `t_channel`;
+CREATE TABLE t_channel (
+		id					INT AUTO_INCREMENT
+	,	channel_name		VARCHAR(20)
+	,	created_user		VARCHAR(100)
+	,	delete_flog			TINYINT(1)
+	,	CREATED_DATETIME timestamp not null default current_timestamp
+	,	primary KEY (id)
+	,	INDEX(id)
+) ENGINE=InnoDB character set utf8mb4;
+
+-- サンプルインサート
 INSERT INTO `t_comment` VALUES ('1', 'sample001', 'A', 'サンプルコメント001');
 INSERT INTO `t_comment` VALUES ('2', 'sample002', 'A', 'サンプルコメント002');
 INSERT INTO `t_comment` VALUES ('3', 'sample003', 'B', 'サンプルコメント003');
